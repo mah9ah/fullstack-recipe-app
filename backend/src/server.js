a34c8ff11983fe87599df99f2d92e479f7c8f830
sediqullah <mahnah@virginia.edu>
2025-08-02 20:20:56 -0400

    small changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import {ENV} from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 const app = express()
 const PORT = ENV.PORT || 5001
 
@@ -36,6 +37,43 @@ app.post("/api/favorites", async (req,res) => {
     }
 
 })
+
+app.get("/api/favorites/:userId", async (req, res) => {
+    try {
+      const { userId } = req.params;
+  
+      const userFavorites = await db
+        .select()
+        .from(favoritesTable)
+        .where(eq(favoritesTable.userId, userId));
+  
+      res.status(200).json(userFavorites);
+    } catch (error) {
+      console.log("Error fetching the favorites", error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+    try {
+      const { userId, recipeId } = req.params;
+  
+      await db
+        .delete(favoritesTable)
+        .where(
+          and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+        );
+  
+      res.status(200).json({ message: "Favorite removed successfully" });
+    } catch (error) {
+      console.log("Error removing a favorite", error);
+      res.status(500).json({ error: "Something went wrong" });
+    }
+  });
+
+
+
 
 app.listen(PORT, () => {
     console.log("server is running on PORT:", PORT)
