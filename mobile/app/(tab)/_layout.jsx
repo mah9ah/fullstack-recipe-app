3c8fbc0909ecbaa3682864bfb336b7b43d6f07bc
sediqullah <mahnah@virginia.edu>
2025-08-04 14:26:51 -0400

    doing the setup for authentication

---
+++ mobile/app/(tab)/_layout.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack, Redirect } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo"
+
+
+const TabsLayout = () => {
+    const { isSignedIn } = useAuth ();
+
+     if ( !isSignedIn ) return < Redirect href={"/(auth)/sign-in"} />;
+  return (
+   <Stack />
+  )
+}
+
+export default TabsLayout;
